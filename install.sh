64e4f39f8615be04174698bc9a38507a16735b56
Jan Kossmann <jan.kossmann@hpi.de>
2021-04-06 14:55:31 +0200

    Initial commit.
    State of Sprint 1 + improvements (65e9f8a).

---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+if [[ -z $OPOSSUM_HEADLESS_SETUP ]]; then
+    read -p 'This script installs the dependencies of Hyrise. It might upgrade already installed packages. Continue? [y|n] ' -n 1 -r < /dev/tty
+else
+    REPLY="y"
+fi
+
+echo
+if echo $REPLY | grep -E '^[Yy]$' > /dev/null; then
+    unamestr=$(uname)
+    if [[ "$unamestr" == 'Darwin' ]]; then
+        if [ ! -d "/Applications/Xcode.app/" ]; then
+            echo "You need to install Xcode from the App Store before proceeding"
+            exit 1
+        fi
+
+        # Needed for proper building under macOS
+        xcode-select --install
+
+        brew --version 2>/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+        echo "Installing dependencies (this may take a while)..."
+        if brew update >/dev/null; then
+            # python2.7 is preinstalled on macOS
+            # check, for each programme individually with brew, whether it is already installed
+            # due to brew issues on MacOS after system upgrade
+            for formula in boost cmake pkg-config parallel; do
+                # if brew formula is installed
+                if brew ls --versions $formula > /dev/null; then
+                    continue
+                fi
+                if ! brew install $formula; then
+                    echo "Error during brew formula $formula installation."
+                    exit 1
+                fi
+            done
+
+            if ! brew install llvm; then
+                echo "Error during llvm/clang installation."
+                exit 1
+            fi
+
+            if ! git submodule update --jobs 5 --init --recursive; then
+                echo "Error during installation."
+                exit 1
+            fi
+        else
+            echo "Error during installation."
+            exit 1
+        fi
+    elif [[ "$unamestr" == 'Linux' ]]; then
+        if [ -f /etc/lsb-release ] && cat /etc/lsb-release | grep DISTRIB_ID | grep Ubuntu >/dev/null; then
+            echo "Installing dependencies (this may take a while)..."
+            if sudo apt-get update >/dev/null; then
+                boostall=$(apt-cache search --names-only '^libboost1.[0-9]+-all-dev$' | sort | tail -n 1 | cut -f1 -d' ')
+                sudo apt-get install --no-install-recommends -y build-essential cmake gcovr parallel $boostall &
+
+                if ! git submodule update --jobs 5 --init --recursive; then
+                    echo "Error during installation."
+                    exit 1
+                fi
+
+                wait $!
+                apt=$?
+                if [ $apt -ne 0 ]; then
+                    echo "Error during installation."
+                    exit 1
+                fi
+            else
+                echo "Error during installation."
+                exit 1
+            fi
+        else
+            echo "Unsupported system. You might get the install script to work if you remove the '/etc/lsb-release' line, but you will be on your own."
+            exit 1
+        fi
+    else
+        echo "Unsupported operating system $unamestr."
+        exit 1
+    fi
+fi
+
+exit 0
---
+++ scripts/format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+unamestr=$(uname)
+if [[ "$unamestr" == 'Darwin' ]]; then
+	format_cmd="clang-format -i -style=file '{}'"
+elif [[ "$unamestr" == 'Linux' ]]; then
+	format_cmd="clang-format-6.0 -i -style=file '{}'"
+fi
+
+
+if [ "${1}" = "all" ]; then
+    find src -iname "*.cpp" -o -iname "*.hpp" | xargs -I{} sh -c "${format_cmd}"
+elif [ "$1" = "modified" ]; then
+    # Run on all changed as well as untracked cpp/hpp files, as compared to the current HEAD. Skip deleted files.
+    { git diff --diff-filter=d --name-only & git ls-files --others --exclude-standard; } | grep -E "^src.*\.[ch]pp$" | xargs -I{} sh -c "${format_cmd}"
+else
+    # Run on all changed as well as untracked cpp/hpp files, as compared to the current master. Skip deleted files.
+    { git diff --diff-filter=d --name-only master & git ls-files --others --exclude-standard; } | grep -E "^src.*\.[ch]pp$" | xargs -I{} sh -c "${format_cmd}"
+fi
