de76be2c96ac9c8b3ab2f0d4ca7f7f088f229bb5
simonstadlinger <stadlinger.simon@gmail.com>
2021-05-10 15:34:08 +0200

    #12 Add compiling but failing multithreading

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <numeric>
 #include <string>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -114,16 +115,25 @@ void Table::compress_chunk(ChunkID chunk_id) {
          "Attempt to compress chunk that is not yet completely filled.");
 
   std::shared_ptr<Chunk> compressed_chunk = std::make_shared<Chunk>();
+  std::vector<std::thread> column_threads = {};
 
   for (ColumnID column_id = ColumnID{0}; column_id < column_count(); ++column_id) {
-    _compress_column(uncompressed_chunk, compressed_chunk, column_id);
+    std::thread single_column_thread(&Table::_compress_column, this,
+                                     std::ref(uncompressed_chunk), std::ref(compressed_chunk), std::ref(column_id));
+    column_threads.push_back(std::move(single_column_thread));
+  }
+
+  for (std::thread & column_thread: column_threads) {
+    if(column_thread.joinable()) {
+      column_thread.join();
+    }
   }
 
   _chunks[chunk_id] = compressed_chunk;
 }
 
-void Table::_compress_column(Chunk& uncompressed_chunk, std::shared_ptr<Chunk> compressed_chunk,
-                             ColumnID col_id) const {
+void Table::_compress_column(Chunk& uncompressed_chunk, std::shared_ptr<Chunk>& compressed_chunk,
+                             ColumnID& col_id) const {
   const auto& column_segment = uncompressed_chunk.get_segment(col_id);
 
   resolve_data_type(column_type(col_id), [&](const auto data_type_t) {
