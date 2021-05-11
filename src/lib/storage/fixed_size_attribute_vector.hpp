5a90f1bda6bf0cf5b98fc7b8a776dee9dbc16c66
Moritz Spranger <32243674+m0e33@users.noreply.github.com>
2021-05-11 10:10:14 +0200

    Update src/lib/storage/fixed_size_attribute_vector.hpp
    Co-authored-by: Jostafarr <35836237+Jostafarr@users.noreply.github.com>

--- src/lib/storage/fixed_size_attribute_vector.hpp
+++ src/lib/storage/fixed_size_attribute_vector.hpp
@@ -28,8 +28,13 @@ class FixedSizeAttributeVector : public BaseAttributeVector {
     // according to this stackoverflow answer it's considered bad stil to just use the [] operator
     // https://stackoverflow.com/questions/31372809/c-vector-size-is-returning-zero
     // nevertheless, this does not feel right.
-    _attributes.push_back((T)value_id);
-    _attributes[i] = _attributes.back();
+     if (_attributes.begin() + position < _attributes.end()) {
+      _attributes.at(position) = (T)value_id;
+    } else if (_attributes.begin() + position == _attributes.end()){
+      _attributes.push_back((T)value_id);
+    } else {
+      throw std::runtime_error("Out of Bounds"); 
+    }
   }
 
   // returns the number of values
