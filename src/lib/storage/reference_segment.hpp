d4f1946d59a9c7a19a9b18499095825e1226f7f8
simonstadlinger <stadlinger.simon@gmail.com>
2021-05-21 17:18:50 +0200

    #38 Add reference segment implementation except estimage_memory_usage(). Expand tests

--- src/lib/storage/reference_segment.hpp
+++ src/lib/storage/reference_segment.hpp
@@ -13,6 +13,8 @@
 #include "types.hpp"
 #include "utils/assert.hpp"
 #include "value_segment.hpp"
+#include "resolve_type.hpp"
+
 
 namespace opossum {
 
@@ -22,20 +24,41 @@ class ReferenceSegment : public BaseSegment {
   // creates a reference segment
   // the parameters specify the positions and the referenced segment
   ReferenceSegment(const std::shared_ptr<const Table>& referenced_table, const ColumnID referenced_column_id,
-                   const std::shared_ptr<const PosList>& pos);
+                   const std::shared_ptr<const PosList>& pos)
+      : _ref_table(referenced_table), _ref_col_id(referenced_column_id), _pos_list(pos) {}
 
-  AllTypeVariant operator[](const ChunkOffset chunk_offset) const override;
+  AllTypeVariant operator[](const ChunkOffset chunk_offset) const override {
+      auto position = _pos_list->at(chunk_offset);
+      auto& chunk = _ref_table->get_chunk(position.chunk_id);
+      auto segment = chunk.get_segment(_ref_col_id);
+      return (*segment)[position.chunk_offset];
+  };
 
   void append(const AllTypeVariant&) override { throw std::logic_error("ReferenceSegment is immutable"); };
 
-  ChunkOffset size() const override;
+  ChunkOffset size() const override {
+      return _pos_list->size();
+  }
 
-  const std::shared_ptr<const PosList>& pos_list() const;
-  const std::shared_ptr<const Table>& referenced_table() const;
+  const std::shared_ptr<const PosList>& pos_list() const {
+      return _pos_list;
+  }
+  const std::shared_ptr<const Table>& referenced_table() const {
+      return _ref_table;
+  }
 
-  ColumnID referenced_column_id() const;
+  ColumnID referenced_column_id() const {
+      return _ref_col_id;
+  }
 
-  size_t estimate_memory_usage() const final;
+  size_t estimate_memory_usage() const final {
+    return 0;
+  }
+
+ protected:
+  const std::shared_ptr<const Table>& _ref_table;
+  const ColumnID _ref_col_id;
+  const std::shared_ptr<const PosList>& _pos_list;
 };
 
 }  // namespace opossum
